de312e4a370c5f53625418689dc7dd4545a9932e
viviankailany <viviankailanymarques@gmail.com>
2025-10-30 16:03:09 -0300

    atualizando uri de dev.

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V10.0_2025.10.30__uri.sql
+++
@@ -1,9 +0,0 @@
-UPDATE metadatavalue mv
-SET text_value = REPLACE(mv.text_value,
-                         'http://localhost:4000',
-                         'https://dspace.dev.interacoes.ltap.ifce.edu.br')
-    FROM metadatafieldregistry mfr
-WHERE mv.metadata_field_id = mfr.metadata_field_id
-  AND mfr.element = 'identifier'
-  AND mfr.qualifier = 'uri'
-  AND mv.text_value LIKE '%http://localhost:4000%';
